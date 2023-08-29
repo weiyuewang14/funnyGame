fcce5a6c1e2065fc8fcfe08be9eb026f7ede1472
Yue <weiyuewang14@163.com>
2023-08-29 14:56:37 +0800

    init my small game program

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <router-view/>
+</template>
+<script>
+// @ is an alias to /src
+import Home from '@/views/Home.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Home
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
